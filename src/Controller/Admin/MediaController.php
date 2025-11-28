5b54eaf0c7d76bfe6cdbcdc5f35c43500a533931
Antoine LRK <55787169+antoinelrk@users.noreply.github.com>
2025-11-28 16:56:49 +0100

    add web_profiler.yaml and update routing annotations to attributes

--- src/Controller/Admin/MediaController.php
+++ src/Controller/Admin/MediaController.php
@@ -6,13 +6,11 @@ use App\Entity\Media;
 use App\Form\MediaType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class MediaController extends AbstractController
 {
-    /**
-     * @Route("/admin/media", name="admin_media_index")
-     */
+    #[Route("/admin/media", name: "admin_media_index")]
     public function index(Request $request)
     {
         $page = $request->query->getInt('page', 1);
@@ -38,9 +36,7 @@ class MediaController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/admin/media/add", name="admin_media_add")
-     */
+    #[Route("/admin/media/add", name: "admin_media_add")]
     public function add(Request $request)
     {
         $media = new Media();
@@ -62,9 +58,7 @@ class MediaController extends AbstractController
         return $this->render('admin/media/add.html.twig', ['form' => $form->createView()]);
     }
 
-    /**
-     * @Route("/admin/media/delete/{id}", name="admin_media_delete")
-     */
+    #[Route("/admin/media/delete/{id}", name: "admin_media_delete")]
     public function delete(int $id)
     {
         $media = $this->getDoctrine()->getRepository(Media::class)->find($id);
