644bd22139fb31833442a7824591613c460df638
Antoine LRK <55787169+antoinelrk@users.noreply.github.com>
2025-12-01 07:23:24 +0100

    add logout functionality and update admin navigation links

--- src/Controller/Admin/SecurityController.php
+++ src/Controller/Admin/SecurityController.php
@@ -26,4 +26,10 @@ final class SecurityController extends AbstractController
             'error'         => $error,
         ]);
     }
+
+    #[Route(path: '/logout', name: 'admin_logout')]
+    public function logout(): Response
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
 }
